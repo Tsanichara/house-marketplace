84fef222bb4c5abe3321c09e4bf5416aca95f884
Tyler Sanichara <Tsanichara67@gmail.com>
2023-06-17 14:45:22 -0400

    Set up profile page and added private route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
@@ -18,7 +19,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore/>}></Route>
           <Route path="/offers" element={<Offers/>}></Route>
-          <Route path="/profile" element={<Profile/>}></Route>
+          <Route path="/profile" element={<PrivateRoute/>}>
+            <Route path="/profile" element={<Profile/>}></Route>
+          </Route>
           <Route path="/sign-in" element={<SignIn/>}></Route>
           <Route path="/sign-up" element={<SignUp/>}></Route>
           <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
