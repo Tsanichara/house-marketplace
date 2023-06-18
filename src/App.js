71046dfb065c831da51eae111b86b0f62f021621
Tyler Sanichara <Tsanichara67@gmail.com>
2023-06-17 20:42:31 -0400

    Created pages for renting and selling and linked them to explore page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
+import Category from "./pages/Category";
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore/>}></Route>
           <Route path="/offers" element={<Offers/>}></Route>
+          <Route path="/category/:categoryName" element={<Category/>}></Route>
           <Route path="/profile" element={<PrivateRoute/>}>
             <Route path="/profile" element={<Profile/>}></Route>
           </Route>
