35a28f7f5fbf090121224b7c69e27a41d06daa99
Tyler Sanichara <Tsanichara67@gmail.com>
2023-06-19 15:07:11 -0400

    Set up offers page and created component for listing item

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { collection, getDocs, query, where, orderBy, limit, startAfter } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
+import ListingItem from "../components/ListingItem";
 
 function Category() {
     const [listings, setListings] = useState(null);
@@ -55,7 +56,7 @@ function Category() {
         <>
         <main>
             <ul className="categoryListings">
-                {listings.map((listing) => (<h3 key={listing.id}>{listing.data.name}</h3>))}
+                {listings.map((listing) => (<ListingItem listing={listing.data} id={listing.id} key={listing.id}></ListingItem>))}
             </ul>
         </main>
         </>)
